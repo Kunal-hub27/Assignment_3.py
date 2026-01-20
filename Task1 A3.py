95746dc887cd9dfca16e3c7f67d631934d3f383b
Kunal-hub27 <kunaalaradhya2007@gmail.com>
2026-01-20 18:16:40 +0530

    Add files via upload

---
+++ Task1 A3.py
@@ -0,0 +1,12 @@
+num=int(input("Enter a number: "))
+def factorial(num):
+    factorial=1
+    while num>1:
+        factorial *= num
+        num -=1
+    return factorial
+print(f"Factorial of {num} is {factorial(num)}")
+
+factorial(num)
+
+
---
+++ Task2 A3.py
@@ -0,0 +1,11 @@
+import math
+num=int(input("Enter a number: "))
+
+output=math.sqrt(num)
+print(f"Square root of {num} is {output}")
+
+output1=math.log(num)
+print(f"Logarithm : {output1}")
+
+output2=math.sin(num)
+print(f"Sine : {output2}")
